03854091b13e9c4644c85c66599271ed67b51d31
bossambani <bossambaka@gmail.com>
2026-03-05 13:04:53 +0300

    user feature

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,22 @@
-from django.contrib.auth.forms import UserCreationForm
+# users/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'role')
+        fields = ['username', 'role', 'password1', 'password2']  # password1/2 handled by UserCreationForm
+
+    # Optional: customize widgets
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['role'].widget.attrs.update({'class': 'form-select'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+
+
+# Optional: password reset form if you want to customize it
+class CustomPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
