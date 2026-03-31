962dfc517eca1a53248678d06d0357fd9052ed92
bossambani <bossambaka@gmail.com>
2026-03-31 12:47:20 +0300

    user

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 # users/urls.py
 from django.urls import path
-from .views import register_view, login_view, logout_view, password_reset_view, admin_dashboard, organizer_dashboard, attendee_dashboard, profile, update_profile
+from .views import register_view, login_view, logout_view, password_reset_view, admin_dashboard, organizer_dashboard, attendee_dashboard, profile, update_profile, admin_dashboard, admin_events, admin_users, change_user_role, export_users_csv, generate_report, delete_user
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -33,3 +33,10 @@ urlpatterns = [
     ),
 
 ]
+
+path('admin/users/', admin_users, name='admin_users'),
+path('admin/events/', admin_events, name='admin_events'),
+path('admin/report/', generate_report, name='admin_report'),
+path('admin/export-users/', export_users_csv, name='export_users'),
+path('admin/change-role/<int:user_id>/', change_user_role, name='change_user_role'),
+path('admin/delete-user/<int:user_id>/', delete_user, name='delete_user'),
