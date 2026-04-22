df16d60fcc8e248ca08eda95b6fcc74a08ed149d
bossambani <bossambaka@gmail.com>
2026-04-22 23:02:35 +0300

    more features

--- event_system/urls.py
+++ event_system/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,8 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('tickets/', include('tickets.urls')),
     path('notifications/', include('notifications.urls')),
+    path('pricing/', TemplateView.as_view(template_name='pricing.html'), name='pricing'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
